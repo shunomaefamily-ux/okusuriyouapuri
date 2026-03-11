b886bcda2cd158d1a33a5bb6f2cb84af9d99c859
shuno <shun.omaefamilt@gmail.com>
2026-03-11 09:53:54 +0900

    gitでテストの動作確認済み

--- app/src/main/java/com/example/myapplication/data/api/ApiClient.kt
+++ app/src/main/java/com/example/myapplication/data/api/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://railsgirls-psq6.onrender.com/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
